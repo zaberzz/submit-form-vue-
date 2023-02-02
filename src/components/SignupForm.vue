a9d7291635e11cc83701fa061136ff8f135c495f
Saba <saba.sadegh67@gmail.com>
2023-02-02 14:54:50 +0330

    add delete option to skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,7 +12,7 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="removeSkill(skill)">{{ skill }}</span>
     </div>
     <div class="terms">
       <input type="checkbox" required v-model="term" />
@@ -42,6 +42,11 @@ export default {
 
         this.tempSkill = "";
       }
+    },
+    removeSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
     },
   },
 };
@@ -81,5 +86,17 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background-color: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
